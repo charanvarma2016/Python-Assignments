a4123397db993d86ee3c4fddbfe48a39da17fa2e
charanvarma2016 <charanvarma2016@gmail.com>
2025-06-04 19:34:18 -0400

    Add files via upload

---
+++ Q7.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  4 17:29:28 2025
+
+@author: saikr
+"""
+
+# Given a tuple of numbers, count how many times the number 3 appears.
+
+# Given tuple
+numbers = (1, 3, 5, 3, 7, 3, 9)
+
+# Count occurrences of 3
+count_3 = numbers.count(3)
+
+# Print the result
+print("Number 3 appears:", count_3, "times")
